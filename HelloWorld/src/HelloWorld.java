46ec9172ad9108bd35d18bf20f1712ca333def7e
Marc Miller <marc@marcs-mbp.speedport_w_921v_1_39_000>
2015-10-25 14:51:50 +0100

    Commit for Exercise1

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -11,8 +11,10 @@ public class HelloWorld {
 	/**
 	 * @param args
 	 */
+	String username;
 	public static void main(String[] args) {
-		System.out.println("Hello ASE2015 - how are you? :)");
+		System.out.println("Hello ASE2015 - how are you now? :)");
+		HelloUser.greetUser();
 	}
 
 }
